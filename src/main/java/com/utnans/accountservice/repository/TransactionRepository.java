c58e6efc6855cfd99a06eaf02401223ed6805e4c
Nauris Utnans <nauris.utnans@gmail.com>
2024-06-09 20:22:06 +0300

    Minor adjustments, cleanup

--- src/main/java/com/utnans/accountservice/repository/TransactionRepository.java
+++ src/main/java/com/utnans/accountservice/repository/TransactionRepository.java
@@ -4,11 +4,12 @@ import com.utnans.accountservice.entity.Transaction;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
     @Query("SELECT tx FROM Transaction tx " +
             "WHERE tx.senderAccount.acctNo = :acctNo OR tx.receiverAccount.acctNo = :acctNo " +
             "ORDER BY tx.execDateTime DESC")
